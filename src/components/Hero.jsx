0857362b0006fe13a8e5403710716a8cfac21a00
Greg Bluwell <greg.bluwell@proton.me>
2023-05-03 13:08:43 +0200

    responsivenss *Hero Slogan @media

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -4,23 +4,28 @@ import GetStarted from "./GetStarted";
 
 const Hero = () => {
   return (
-    <section id="home" className={`flex md:flex-row flex-col ${styles.paddingY}`}>
-      <div className={`flex-1 ${styles.flexStart} flex-col xl:px-0 sm:px-16 px-6`}>
+    <section
+      id="home"
+      className={`flex md:flex-row flex-col ${styles.paddingY}`}
+    >
+      <div
+        className={`flex-1 ${styles.flexStart} flex-col xl:px-0 sm:px-16 px-6`}
+      >
         <div className="flex flex-row items-center py-[6px] px-4 bg-discount-gradient rounded-[10px] mb-2">
           <img src={discount} alt="discount" className="w-[48px] h-[48px]" />
           <p className={`${styles.paragraph} ml-2`}>
-            <span className="text-white">-20%</span> offert pendant {" "}
+            <span className="text-white">-20%</span> offert pendant{" "}
             <span className="text-white">1 mois</span>
           </p>
         </div>
 
-        <div className="flex flex-row justify-between items-center w-full">
+        <div className="slogan-container flex flex-row justify-between items-center w-full ">
           <h1 className="flex-1 font-poppins font-semibold ss:text-[72px] text-[52px] text-white ss:leading-[100.8px] leading-[75px]">
-          <br className="sm:block hidden" />{" "}
-        <h1 className="font-poppins font-semibold ss:text-[68px] text-[52px] text-white ss:leading-[100.8px] leading-[75px] w-full">
-          La Méthode de Paiement
-        </h1>
-             <span className="text-gradient"> Nouvelle Generation</span>{" "}
+            <br className="sm:block hidden" />{" "}
+            <h1 className="font-poppins font-semibold ss:text-[68px] text-[52px] text-white ss:leading-[100.8px] leading-[75px] w-full">
+              La Méthode de Paiement
+            </h1>
+            <span className="text-gradient"> Nouvelle Generation</span>{" "}
           </h1>
           <div className="ss:flex hidden md:mr-4 mr-0">
             <GetStarted />
@@ -28,13 +33,20 @@ const Hero = () => {
         </div>
 
         <p className={`${styles.paragraph} max-w-[470px] mt-5`}>
-        Nos experts utilisent une méthodologie d'identification de carte répondant à vos besoins.
-        Nous examinons les taux d'intérêt annuels, les frais annuels.
+          Nos experts utilisent une méthodologie d'identification de carte
+          répondant à vos besoins. Nous examinons les taux d'intérêt annuels,
+          les frais annuels.
         </p>
       </div>
 
-      <div className={`flex-1 flex ${styles.flexCenter} md:my-0 my-10 relative`}>
-        <img src={robot} alt="billing" className="w-[100%] h-[100%] relative z-[5]" />
+      <div
+        className={`flex-1 flex ${styles.flexCenter} md:my-0 my-10 relative`}
+      >
+        <img
+          src={robot}
+          alt="billing"
+          className="object-scale-100 min-w-[500px] relative z-[5]"
+        />
 
         {/* gradient start */}
         <div className="absolute z-[0] w-[40%] h-[35%] top-0 pink__gradient" />
--- src/style.js
+++ src/style.js
@@ -27,3 +27,4 @@ export const layout = {
 };
 
 export default styles;
+
